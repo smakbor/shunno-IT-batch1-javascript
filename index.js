c4ec5d082749848a1143e65b33649d03f4673170
smakbor <smakbar3965@gmail.com>
2024-01-24 21:47:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,125 @@
+//array er moddhe access kora jay 2 vabe ...
+//1--> looping
+//2--> dynamic access
+
+// const number = [2, 3, 4, 7, 8, 4, 12];
+// // console.log(number.length - 1);
+
+// console.log(number[4]);
+// // for(initialization;condition;increment){
+// //     // code execution block
+// // }
+
+// for (let index = 0; index < number.length; index++) {
+//     console.log(number[index]);
+// }
+
+// const num = [3, 6, 7, 4, 90, 23, 14, 45];
+// const result = num.slice(1, 5);
+// console.log(result);
+// console.log(num);
+
+function printName(name) {
+    let x = 10;
+    let y = 5;
+    let z = x + y;
+    console.log(z);
+    console.log(name);
+}
+// printName("Mehrab Hossen");
+
+// function myFunc(p1, p2) {
+//     const sum = p1 + p2;
+//     return sum;
+// }
+// const result = myFunc(5, 3);
+// const result2 = myFunc(6, 8);
+// console.log(result);
+// console.log(result2);
+
+// const bikeName = ["R15", "Suzuki", "Discover", "Yamaha", "RX-5", "Honda"];
+
+// function printBikeName(bikeArr) {
+//     for (let index = 0; index < bikeArr.length; index++) {
+//         console.log(bikeArr[index]);
+//     }
+// }
+// printBikeName(bikeName);
+
+// const number = [3, 6, 9, 23, 13, 45, 67, 78];
+
+// number.forEach((item) => console.log(item));
+
+// function subFunction(p1, p2) {
+//     const sum = p1 + p2;
+//     return sum;
+// }
+
+// function mainFunction() {
+//     let x = 10;
+//     let y = 5;
+
+//     return subFunction(x, y);
+// }
+
+// const result = mainFunction();
+// console.log(result);
+
+// let number = [4, 6, 8, 9, 12, 11];
+
+// function sum(item) {
+//     console.log(item + 6);
+// }
+
+// function mapFunction(arr, callback) {
+//     let el = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         // let count = callback();
+//         // el.push(callback(arr[i]));
+//         el = callback(arr[i]);
+//     }
+//     return el;
+// }
+
+// let result = mapFunction(number, sum);
+// console.log(result);
+
+// data type
+
+// let name = "Rahim";
+
+// let number = 8;
+// function studentName(num) {
+//     // parameter receive korte hoy
+//     // console.log(num);
+//     //code execution block
+//     let y = 7;
+//     // console.log(number);
+//     let info = {
+//         name: "rahim",
+//         age: 23,
+//     };
+
+//     return info;
+// }
+
+// console.log(number);
+
+// let result = studentName(); //argument pass korte hoy
+// console.log(result["age"]);
+
+// let akbor = {
+//     age: 25,
+//     roll: 12231,
+//     cgpa: 3.0,
+//     weight: 50,
+//     favColor: "white",
+// };
+
+// let Yeakub = {
+//     age: 25,
+//     roll: 231412,
+//     cgpa: 3.04,
+//     weight: 55,
+//     favColor: "red",
+// };
